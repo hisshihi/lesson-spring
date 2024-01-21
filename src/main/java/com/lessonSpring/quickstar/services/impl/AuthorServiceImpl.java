3e128878180e2fcb7f970ae410f18bab0532ca23
HissDev <hissoffc@gmail.com>
2024-01-21 19:02:58 +0300

    Написал метод частичного обновления автора. 	* Для этого создал контроллер с методом path, чтобы частично обновить автора. 	* Далее создал метод patrialUpdate, который принимает id автора и самого автора. 	* Создал этот метод в интерфесе. 	* Написал этот метод в классе реализации методов интерфейса. 		* Для начала устанавливаю id который приходт с http запросом. 		* Далее идёт проверка, есть ли такой id. 		* Затем проверка, существует ли значение поля name, если оно существует, то оно присваивается полю name объекта existingAuthor. 			Optional.ofNullable(authorEntity.getName()).ifPresent(existingAuthor::setName); 			Эта инструкция проверяет, не равно ли поле name объекта authorEntity null. Если оно не равно null, то метод setName() вызывается с параметром, равным значению поля name объекта authorEntity. 		Тоже самое и для age. 	После того, как поля existingAuthor были обновлены он сохраняется в бд.

--- src/main/java/com/lessonSpring/quickstar/services/impl/AuthorServiceImpl.java
+++ src/main/java/com/lessonSpring/quickstar/services/impl/AuthorServiceImpl.java
@@ -51,5 +51,16 @@ public class AuthorServiceImpl implements AuthorService {
         return authorRepository.existsById(id);
     }
 
+    @Override
+    public AuthorEntity patrialUpdate(Long id, AuthorEntity authorEntity) {
+        authorEntity.setId(id);
+
+        return authorRepository.findById(id).map(existingAuthor -> {
+            Optional.ofNullable(authorEntity.getName()).ifPresent(existingAuthor::setName);
+            Optional.ofNullable(authorEntity.getAge()).ifPresent(existingAuthor::setAge);
+            return authorRepository.save(existingAuthor);
+        }).orElseThrow(() -> new RuntimeException("Автор не существует"));
+    }
+
 
 }
