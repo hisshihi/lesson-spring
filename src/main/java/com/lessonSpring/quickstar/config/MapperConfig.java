81f30fd45157a1e4690c46260139b8b132ef1442
HissDev <hissoffc@gmail.com>
2024-01-22 22:05:01 +0300

    Добавил код благодаря которому объекты сопостовляются более гибко и при создании книги автор теперь не null а можно указать именно самого автора

--- src/main/java/com/lessonSpring/quickstar/config/MapperConfig.java
+++ src/main/java/com/lessonSpring/quickstar/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package com.lessonSpring.quickstar.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +10,16 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        /*
+        * Это означает, что ModelMapper будет использовать более гибкую стратегию сопоставления при сопоставлении объектов.
+        *  В частности, ModelMapper будет пытаться сопоставить поля, даже если они имеют разные имена или типы.
+
+        *  В общем, этот код создает и возвращает объект ModelMapper с настройкой MATCHING_STRATEGY на LOOSE.
+        *  Это позволяет ModelMapper сопоставлять объекты более гибким образом.
+        * */
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
 
 }
